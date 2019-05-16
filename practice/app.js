a6e8eb85ce7d41d43c816a57302ef66c43dea5f1
Arnold <arnold.pires92@gmail.com>
2019-05-15 20:34:22 -0400

    more of that node.js lyfe

---
+++ practice/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('<h1>Hello Arnold!</h1><p>How are you?</p><p>I am doing well</p>'))
+app.use(express.static('views'))
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});

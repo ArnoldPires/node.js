679ce86a39f86e3a680292ad20ccc3e460acc4ea
Arnold <arnold.pires92@gmail.com>
2019-05-20 20:39:33 -0400

    finished the project, but still need to solve the change color problem

--- webnotes/app.js
+++ webnotes/app.js
@@ -1,17 +1,21 @@
 const myNotes = [
-  'http is a protocol',
-  'http requests have a url, method, header, and body',
-  'I like peaches'
+
 ];
-
 const express = require('express')
-const app = express()
+const app = express();
 const port = 3000
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/css', express.static('css'));
 //app.get('/webnotes', (req, res) => res.send(''))
 //app.use('/', express.static('views'));
 app.get('/', (req, res) => {
   res.render('notes.ejs', { notes: myNotes});
 });
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/notes', (req, res) => {
+  myNotes.push(req.body.note);
+  res.redirect('/');
+});
+app.listen(port, () => {
+    console.log(`Do as I say and work! ${port}!`)
+});

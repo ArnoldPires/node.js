210d3208bbd5a71370512e56606b8dd348d465ce
Arnold <arnold.pires92@gmail.com>
2019-05-21 20:39:04 -0400

    not finished

--- no-js/homework.js
+++ no-js/homework.js
@@ -3,3 +3,8 @@ const app = express();
 app.listen(3000, function() { console.log ('Homework app listening on port 3000');
 });
 app.get('/', (req, res) => res.send('This is my CodeSquad homework.'));
+app.get('/', (req, res) => {
+  let judges = [
+    {judge: 'Judge Dredd' rank: }
+  ]
+})
